076aa565ed92f67bb29d6b44cc2aa496b3aa9a0f
shrimp <sehun0518@naver.com>
2025-04-14 00:14:13 +0900

    Real Initial commit

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import Image from "next/image";
 
 const navItems = [
   { label: "Dashboard", href: "/", id: "dashboard" },
